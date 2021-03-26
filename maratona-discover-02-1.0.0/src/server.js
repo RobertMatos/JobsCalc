52cef9486b094f79caf26d439b8785dbaa0368c7
Robert Matos <robertvmatos@gmail.com>
2021-03-26 13:20:42 -0300

    Estabelecendo arquivos statics
    Estabelecendo a pasta public como estática

--- maratona-discover-02-1.0.0/src/server.js
+++ maratona-discover-02-1.0.0/src/server.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const server = express()
 
+//Fica no meio da chamada e a resposta
+//O use serve para você adicionar configurações ao seu servidor
+//Habilitar arquivos static
+server.use(express.static("public"))
+
 //Resquest, response
 server.get('/', (request, response) => {
    return response.sendFile(__dirname + "/views/index.html")
